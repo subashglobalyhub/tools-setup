932ebad6ea20ac27ebec9627a57772071826fe2e
subashglobalyhub <subash.chaudhary@globalyhub.com>
2024-06-25 10:54:10 +0545

    Adding scripts: 2

--- initial-setup.sh
+++ initial-setup.sh
@@ -10,9 +10,49 @@ function print_header {
     figlet -c -f slant "$1"
     print_separator
 }
+
+# Detection in Yellow color
+function print_init {
+    local message="$1"
+    printf "\e[33m%s\e[0m\n" "$message"  
+}
+
+# Intermediate in Blue color
+function print_intermediate {
+    local message="$1"
+    printf "\e[34m%s\e[0m\n" "$message"  
+}
+
+# Completion in Green color
+function print_success {
+    local message="$1"
+    printf "\e[1m\e[32m%s\e[0m\n" "$message"  
+    print_separator
+}
+
+# Failures in Red color
+function print_fail {
+    local message="$1"
+    printf "\e[1m\e[31m%s\e[0m\n" "$message"  
+    print_separator
+}
+check_figlet_installed() {
+    print_separator
+    print_header "1 - FIGLET"
+    print_separator
+    sleep 2
+    if command -v figlet &>/dev/null; then
+        print_success "Figlet is already installed"  
+        return 0
+    else
+        print_fail "Figlet package is not found in system"
+        return 1
+    fi
+}
+
 check_make_installed() {
     print_separator
-    print_header "3 - make"
+    print_header "2 - make"
     print_separator
     sleep 2
     if command -v make &>/dev/null; then
@@ -22,6 +62,22 @@ check_make_installed() {
         echo -e "make is not found in system" 
         return 1
     fi
+}
+install_figlet() {
+    if check_figlet_installed; then
+        return
+    fi
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
+    if grep -q 'Ubuntu' /etc/os-release; then
+        print_init "Initializing Figlet installation process"
+        sudo apt-get update
+        sudo apt-get install -y figlet
+    else
+        echo "Unsupported Linux distribution"
+        exit 1
+    fi
+    print_separator
 }
 install_make() {
     if check_make_installed; then
@@ -44,7 +100,8 @@ install_make() {
     print_separator
 }
 main() {
-install_make
+    install_figlet
+    install_make
 }
 
 main 
